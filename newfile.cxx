b3316074054e34f079aa0f54c992b423f9c9c171
irungupatrick144-dev <irungupatrick144@gmail.com>
2025-10-03 08:46:44 +0300

    Students eligibility

---
+++ newfile.cxx
@@ -0,0 +1,19 @@
+/* 
+name: patrick kuria
+reg:CT101/G/26479/25
+descipition:check eligibility of student in exam
+*/
+#include<stdio.h>
+int main()}
+float attendance,&average marks;
+// prompt the user toventer attendance and average marks in percdntage
+printf("enter attendance=70%:");
+scanf("%f,&attendance");
+// prompt the user to enter avarage  marks 
+printf(" enter avarege marks =40:");
+scan"%,&attendance");
+// eligibility
+	if(attendance>75,&&averagemarks>40){
+		printf("eligible for final exam:\n"); else(
+		printf(" not eligible for final exam:")!)
+}

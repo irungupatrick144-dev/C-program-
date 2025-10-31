ec33c68295de8f5b6af21680c05951ae26d64361
irungupatrick144-dev <irungupatrick144@gmail.com>
2025-10-31 09:04:11 +0300

    Add files via upload

---
+++ newfile.cxx
@@ -0,0 +1,38 @@
+/*
+name:patrick kuria
+reg:CT101/G/26479/25
+description:fuctions
+*/
+#include <stdio.h>
+
+float calculateElectricBill(int units)
+{
+	float bill = 0.0;
+
+	if (units <= 100)
+	{
+		bill = units * 10;
+	}
+	else if (units <= 200)
+	{
+		bill = (100 * 10) + (units - 100) * 15;
+	}
+	else
+	{
+		bill = (100 * 10) + (100 * 15) + (units - 200) * 20;
+	}
+
+	return bill;
+}
+
+int main()
+{
+	int units;
+	printf("Enter the number of units consumed: ");
+	scanf("%d", &units);
+
+	float totalBill = calculateElectricBill(units);
+	printf("Total Electricity Bill: KSh. %.2f\n", totalBill);
+
+	return 0;
+}
